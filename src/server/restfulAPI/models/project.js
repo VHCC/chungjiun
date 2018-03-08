2f756010bb689fbd8a22f879a4073f5d7166c478
Ichen <ichenJSW@jswpac.com>
2018-03-08 18:06:30 +0800

    project Model update

--- src/server/restfulAPI/models/project.js
+++ src/server/restfulAPI/models/project.js
@@ -1,17 +1,44 @@
 var mongoose = require('mongoose');
 
 module.exports = mongoose.model('Project', {
+    //編碼方式
+    // prjCode = year + code + type + 流水號
+    // 年度
+    year: {
+        type: String
+    },
+    // 總案號
     code: {
         type: String
     },
+    // 類型：總案、規劃、設計、監造、服務
+    type: {
+        type: String
+    },
+    // 專案編碼
+    prjCode: {
+        type: String
+    },
+    // 專案名稱
     name: {
         type: String
     },
+    // 承辦人員
     majorID: {
         type: String
     },
-    creator: {
-        type: String
+    // 協辦人員
+    workers : {
+        type: Array,
+        default: []
+    },
+    //技師
+    technician: {
+        type: Array,
+        default: []
+    },
+    enable : {
+        type: Boolean
     }
 
 });
