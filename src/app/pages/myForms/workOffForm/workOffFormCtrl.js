ec2edac15adc3c67c8e1534fa53508690c0d7a22
Ichen <ichenJSW@jswpac.com>
2018-03-15 17:19:56 +0800

    work off form update

--- src/app/pages/myForms/workOffForm/workOffFormCtrl.js
+++ src/app/pages/myForms/workOffForm/workOffFormCtrl.js
@@ -9,21 +9,33 @@
         .controller('workOffFormCtrl',
             [
                 '$scope',
+                '$cookies',
                 'Project',
                 WorkOffFormCtrl
             ]);
 
     /** @ngInject */
     function WorkOffFormCtrl($scope,
-                         Project) {
+                             cookies,
+                             Project) {
+
+        $scope.username = cookies.get('username');
 
         Project.findAll()
             .success(function (allProjects) {
-                console.log(JSON.stringify(allProjects));
-                vm.projects = allProjects;
+                console.log(allProjects);
+                cC.projects = allProjects;
             });
 
-        var vm = this;
+        var cC = this;
+
+        cC.offTypes = [
+            {label: '事假',   type: 'w01'},
+            {label: '病假',   type: 'w02'},
+            {label: '特休假', type: 'w03'},
+            {label: '補休',   type: 'w04'},
+        ];
+
 
     }
 })();
