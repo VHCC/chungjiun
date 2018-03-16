531353865d684b3809e23832d767215282296d46
Ichen <ichenJSW@jswpac.com>
2018-03-16 18:56:46 +0800

    add userEdit module and update Todo page hide

--- src/app/pages/myNewPage/myNewPage.module.js
+++ src/app/pages/myNewPage/myNewPage.module.js
@@ -7,16 +7,16 @@
     /** @ngInject */
     function routeConfig($stateProvider) {
         $stateProvider
-            .state('myNewPage', {
-                url: '/myNewPage',
-                templateUrl: 'app/pages/myNewPage/my-new-page.html',
-                controller: 'mainController',
-                title: 'My Todo',
-                sidebarMeta: {
-                    icon: 'ion-card',
-                    order: global.pages.myNewPage,
-                },
-            })
+            // .state('myNewPage', {
+            //     url: '/myNewPage',
+            //     templateUrl: 'app/pages/myNewPage/my-new-page.html',
+            //     controller: 'mainController',
+            //     title: 'My Todo',
+            //     sidebarMeta: {
+            //         icon: 'ion-card',
+            //         order: global.pages.myNewPage,
+            //     },
+            // })
     }
 
 })();
