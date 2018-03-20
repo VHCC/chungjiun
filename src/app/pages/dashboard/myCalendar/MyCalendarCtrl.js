98bbd987bb3febee39c2b20006b11ac5a1169deb
Ichen.tsu <r99521320@gmail.com>
2018-03-21 04:49:25 +0800

    my calendar bug fixes

--- src/app/pages/dashboard/myCalendar/MyCalendarCtrl.js
+++ src/app/pages/dashboard/myCalendar/MyCalendarCtrl.js
@@ -3,81 +3,81 @@
  * created on 16.12.2015
  */
 (function () {
-    'use strict';
+        'use strict';
 
-    angular.module('BlurAdmin.pages.dashboard')
-        .controller('MyCalendarCtrl',
-            [
-                '$scope',
-                'baConfig',
-                'Project',
-                'DateUtil',
-                'ProjectUtil',
-                MyCalendarCtrl
-            ]);
+        angular.module('BlurAdmin.pages.dashboard')
+            .controller('MyCalendarCtrl',
+                [
+                    '$scope',
+                    'baConfig',
+                    'Project',
+                    'DateUtil',
+                    'ProjectUtil',
+                    MyCalendarCtrl
+                ]);
 
-    /** @ngInject */
-    function MyCalendarCtrl($scope,
-                            baConfig,
-                            Project,
-                            DateUtil,
-                            DateUtil) {
+        /** @ngInject */
         function MyCalendarCtrl($scope,
                                 baConfig,
                                 Project,
                                 DateUtil,
                                 ProjectUtil) {
-            var dashboardColors = baConfig.colors.dashboard;
+            function MyCalendarCtrl($scope,
+                                    baConfig,
+                                    Project,
+                                    DateUtil,
+                                    ProjectUtil) {
+                var dashboardColors = baConfig.colors.dashboard;
 
-            Project.findAll()
-                .success(function (allProjects) {
-                    console.log('rep - GET ALL Project, SUCCESS');
-                    console.log(allProjects);
-                    $scope.loading = false;
-                    $scope.projects = allProjects
+                Project.findAll()
+                    .success(function (allProjects) {
+                        console.log('rep - GET ALL Project, SUCCESS');
+                        console.log(allProjects);
+                        $scope.loading = false;
+                        $scope.projects = allProjects
 
-                    var totalEvent = [];
+                        var totalEvent = [];
 
-                    for (var index = 0; index < $scope.projects.length; index++) {
-                        var eventItem = {
-                            title: $scope.projects[index].name + " - " + ProjectUtil.getTypeText($scope.projects[index].type),
-                            start: DateUtil.formatDate($scope.projects[index].endDate),
-                            color: baConfig.colors.warning,
-                        }
-                        totalEvent.push(eventItem);
-                    }
-
-                    // Init Calender
-                    var $element = $('#calendar').fullCalendar({
-                        //height: 335,
-                        header: {
-                            left: 'prev, next today',
-                            center: 'title',
-                            right: 'month,agendaWeek,agendaDay'
-                        },
-                        defaultDate: new Date(),
-                        selectable: false,
-                        selectHelper: false,
-                        select: function (start, end) {
-                            var title = prompt('Event Title:');
-                            var eventData;
-                            if (title) {
-                                eventData = {
-                                    title: title,
-                                    start: start,
-                                    end: end
-                                };
-                                $element.fullCalendar('renderEvent', eventData, true); // stick? = true
+                        for (var index = 0; index < $scope.projects.length; index++) {
+                            var eventItem = {
+                                title: $scope.projects[index].name + " - " + ProjectUtil.getTypeText($scope.projects[index].type),
+                                start: DateUtil.formatDate($scope.projects[index].endDate),
+                                color: baConfig.colors.warning,
                             }
-                            $element.fullCalendar('unselect');
-                        },
-                        editable: false,
-                        eventLimit: true, // allow "more" link when too many events
-                        events: totalEvent,
+                            totalEvent.push(eventItem);
+                        }
+
+                        // Init Calender
+                        var $element = $('#calendar').fullCalendar({
+                            //height: 335,
+                            header: {
+                                left: 'prev, next today',
+                                center: 'title',
+                                right: 'month,agendaWeek,agendaDay'
+                            },
+                            defaultDate: new Date(),
+                            selectable: false,
+                            selectHelper: false,
+                            select: function (start, end) {
+                                var title = prompt('Event Title:');
+                                var eventData;
+                                if (title) {
+                                    eventData = {
+                                        title: title,
+                                        start: start,
+                                        end: end
+                                    };
+                                    $element.fullCalendar('renderEvent', eventData, true); // stick? = true
+                                }
+                                $element.fullCalendar('unselect');
+                            },
+                            editable: false,
+                            eventLimit: true, // allow "more" link when too many events
+                            events: totalEvent,
+                        });
+
                     });
+            }
 
-                });
         }
-    }
-
-)();
+    })();
