418ea7578b2bba49d64d5132d60a67850e8eb255
Ichen <ichenJSW@jswpac.com>
2018-03-21 18:22:01 +0800

    wotk hour table form update

--- src/server/restfulAPI/models/workHourForm.js
+++ src/server/restfulAPI/models/workHourForm.js
@@ -1,12 +1,15 @@
 var mongoose = require('mongoose');
 
 module.exports = mongoose.model('WorkHourForm', {
+    //填表人
     creatorDID: {
       type: String,
     },
+    // 首周
     create_formDate: {
         type: String,
     },
+    // tableDID以及prjDID
     formTables: {
         type: Array,
     },
