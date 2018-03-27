42dd894c270e433ef60b5b76f131288724abac3e
Ichen <ichenJSW@jswpac.com>
2018-03-27 18:23:42 +0800

    add Util MyDatePicker

---
+++ src/app/pages/myInput/widgets/myDatePicker/myDatePickerCtrl.js
@@ -0,0 +1,37 @@
+/**
+ * Created by n.poltoratsky
+ * on 23.06.2016.
+ */
+(function(){
+    'use strict';
+
+    angular.module('BlurAdmin.pages.myInput')
+        .controller('MyDatePickerCtrl', myDatePickerCtrl);
+
+    /** @ngInject */
+    function myDatePickerCtrl($scope) {
+
+        $scope.openDatePicker = openDatePicker;
+        $scope.myDT = new Date();
+        $scope.opened = false;
+        // $scope.formats = ['dd-MMMM-yyyy', 'MM/dd', 'dd.MM.yyyy', 'shortDate'];
+        // $scope.format = $scope.formats[1];
+        $scope.options = {
+            showWeeks: false
+        };
+        $('.myDatePicker').mask('M0/D0', {
+            translation: {
+                'M': {
+                    pattern: /[01]/,
+                },
+                'D': {
+                    pattern: /[0123]/,
+                }
+            }
+        });
+        function openDatePicker() {
+            console.log($scope.format);
+            $scope.opened = true;
+        }
+    }
+})();
