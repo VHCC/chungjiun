a5c3c51c1a3708810bf51467651f6ca98be3a4fa
Ichen <ichenJSW@jswpac.com>
2018-03-27 18:24:01 +0800

    update DateUtil

--- src/app/pages/pages.module.factory.js
+++ src/app/pages/pages.module.factory.js
@@ -172,6 +172,24 @@
                 getShiftDatefromFirstDate: function (firstDate, offset) {
                     var firstDay = moment(firstDate).add(offset, 'days');
                     return firstDay.format('YYYY/MM/DD');
+                },
+                getDay: function (day) {
+                    switch (day) {
+                        case 0:
+                            return "日";
+                        case 1:
+                            return "一";
+                        case 2:
+                            return "二"
+                        case 3:
+                            return "三"
+                        case 4:
+                            return "四"
+                        case 5:
+                            return "五"
+                        case 6:
+                            return "六"
+                    }
                 }
             }
         })
