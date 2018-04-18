db5884ce6a6f739e34813910a2757cddd197956f
Ichen <ichenJSW@jswpac.com>
2018-04-18 17:30:38 +0800

    work hour manager check, related priject feature

--- src/server/restfulAPI/forms/workHourTableForm/workHourTableForm.routes.js
+++ src/server/restfulAPI/forms/workHourTableForm/workHourTableForm.routes.js
@@ -1,5 +1,6 @@
 var WorkHourForm = require('../../models/workHourForm');
 var WorkHourTableForm = require('../../models/workHourTableForm');
+var Project = require('../../models/project');
 
 module.exports = function (app) {
     'use strict';
@@ -28,7 +29,8 @@ module.exports = function (app) {
                     create_formDate: req.body.create_formDate,
                 }
                 findData.push(target);
-            };
+            }
+            ;
             // console.log(findData);
             // 刪除既有 工時表格
             WorkHourTableForm.remove(
@@ -50,44 +52,44 @@ module.exports = function (app) {
                     prjDID: req.body.formTables[index].prjDID,
                     create_formDate: req.body.create_formDate,
                     //MON
-                    mon_hour:       req.body.formTables[index].mon_hour,
-                    mon_memo:       req.body.formTables[index].mon_memo,
-                    mon_hour_add:   req.body.formTables[index].mon_hour_add,
-                    mon_memo_add:   req.body.formTables[index].mon_memo_add,
+                    mon_hour: req.body.formTables[index].mon_hour,
+                    mon_memo: req.body.formTables[index].mon_memo,
+                    mon_hour_add: req.body.formTables[index].mon_hour_add,
+                    mon_memo_add: req.body.formTables[index].mon_memo_add,
                     //TUE
-                    tue_hour:       req.body.formTables[index].tue_hour,
-                    tue_memo:       req.body.formTables[index].tue_memo,
-                    tue_hour_add:   req.body.formTables[index].tue_hour_add,
-                    tue_memo_add:   req.body.formTables[index].tue_memo_add,
+                    tue_hour: req.body.formTables[index].tue_hour,
+                    tue_memo: req.body.formTables[index].tue_memo,
+                    tue_hour_add: req.body.formTables[index].tue_hour_add,
+                    tue_memo_add: req.body.formTables[index].tue_memo_add,
                     //WES
-                    wes_hour:       req.body.formTables[index].wes_hour,
-                    wes_memo:       req.body.formTables[index].wes_memo,
-                    wes_hour_add:   req.body.formTables[index].wes_hour_add,
-                    wes_memo_add:   req.body.formTables[index].wes_memo_add,
+                    wes_hour: req.body.formTables[index].wes_hour,
+                    wes_memo: req.body.formTables[index].wes_memo,
+                    wes_hour_add: req.body.formTables[index].wes_hour_add,
+                    wes_memo_add: req.body.formTables[index].wes_memo_add,
                     //THU
-                    thu_hour:       req.body.formTables[index].thu_hour,
-                    thu_memo:       req.body.formTables[index].thu_memo,
-                    thu_hour_add:   req.body.formTables[index].thu_hour_add,
-                    thu_memo_add:   req.body.formTables[index].thu_memo_add,
+                    thu_hour: req.body.formTables[index].thu_hour,
+                    thu_memo: req.body.formTables[index].thu_memo,
+                    thu_hour_add: req.body.formTables[index].thu_hour_add,
+                    thu_memo_add: req.body.formTables[index].thu_memo_add,
                     //FRI
-                    fri_hour:       req.body.formTables[index].fri_hour,
-                    fri_memo:       req.body.formTables[index].fri_memo,
-                    fri_hour_add:   req.body.formTables[index].fri_hour_add,
-                    fri_memo_add:   req.body.formTables[index].fri_memo_add,
+                    fri_hour: req.body.formTables[index].fri_hour,
+                    fri_memo: req.body.formTables[index].fri_memo,
+                    fri_hour_add: req.body.formTables[index].fri_hour_add,
+                    fri_memo_add: req.body.formTables[index].fri_memo_add,
                     //SAT
-                    sat_hour:       req.body.formTables[index].sat_hour,
-                    sat_memo:       req.body.formTables[index].sat_memo,
-                    sat_hour_add:   req.body.formTables[index].sat_hour_add,
-                    sat_memo_add:   req.body.formTables[index].sat_memo_add,
+                    sat_hour: req.body.formTables[index].sat_hour,
+                    sat_memo: req.body.formTables[index].sat_memo,
+                    sat_hour_add: req.body.formTables[index].sat_hour_add,
+                    sat_memo_add: req.body.formTables[index].sat_memo_add,
                     //SUN
-                    sun_hour:       req.body.formTables[index].sun_hour,
-                    sun_memo:       req.body.formTables[index].sun_memo,
-                    sun_hour_add:   req.body.formTables[index].sun_hour_add,
-                    sun_memo_add:   req.body.formTables[index].sun_memo_add,
+                    sun_hour: req.body.formTables[index].sun_hour,
+                    sun_memo: req.body.formTables[index].sun_memo,
+                    sun_hour_add: req.body.formTables[index].sun_hour_add,
+                    sun_memo_add: req.body.formTables[index].sun_memo_add,
                     //RIGHT
-                    isSendReview:  req.body.formTables[index].isSendReview,
-                    isManagerCheck:  req.body.formTables[index].isManagerCheck,
-                    isExecutiveCheck:  req.body.formTables[index].isExecutiveCheck,
+                    isSendReview: req.body.formTables[index].isSendReview,
+                    isManagerCheck: req.body.formTables[index].isManagerCheck,
+                    isExecutiveCheck: req.body.formTables[index].isExecutiveCheck,
 
                 }, function (err, workTable) {
                     resIndex++;
@@ -142,6 +144,49 @@ module.exports = function (app) {
         });
     });
 
+    // get forms for manager
+    app.post(global.apiUrl.post_work_hour_get_for_manager, function (req, res) {
+        var relatedProjects = [];
+        var prjCount = 0;
+        Project.find(
+            {
+                majorID: req.body.majorID,
+            }, function (err, projects) {
+                relatedProjects = projects;
+                prjCount = projects.length;
+                var findData = [];
+                var prjIDArray = [];
+                for (var index = 0; index < prjCount; index++) {
+                    var target = {
+                        creatorDID: req.body.creatorDID,
+                        create_formDate: req.body.create_formDate,
+                    }
+                    prjIDArray.push(relatedProjects[index]._id)
+                    findData.push(target);
+                }
+
+
+                WorkHourForm.find({
+                    // $or: findData,
+                    creatorDID: req.body.creatorDID,
+                    create_formDate: req.body.create_formDate,
+                }, function (err, forms) {
+                    if (err) {
+                        res.send(err);
+                    }
+                    res.status(200).send({
+                        code: 200,
+                        error: global.status._200,
+                        payload: forms,
+                        prjIDArray: prjIDArray,
+                    });
+                });
+
+            });
+
+
+    })
+
     // find table by tableid array
     app.post(global.apiUrl.post_work_hour_table_find_by_tableid_array, function (req, res) {
         var tableCount = req.body.tableIDArray.length;
@@ -151,7 +196,7 @@ module.exports = function (app) {
                 _id: req.body.tableIDArray[index],
             }
             findData.push(target);
-        };
+        }
 
         var query = {};
         query.$or = findData;
