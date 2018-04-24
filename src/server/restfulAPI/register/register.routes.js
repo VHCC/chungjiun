ba8e726e50942a51c394d69efa2fc1096c2e3109
Ichen <ichenJSW@jswpac.com>
2018-04-24 15:08:36 +0800

    user register feature

--- src/server/restfulAPI/register/register.routes.js
+++ src/server/restfulAPI/register/register.routes.js
@@ -7,6 +7,20 @@ module.exports = function(app) {
 // ----- define routes
     // create
     app.post(global.apiUrl.post_register_user, function(req, res) {
-        console.log(123123)
+        console.log(req.body);
+        userModel.create({
+            name: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            roleType: req.body.roletype,
+        }, function (err, project) {
+            if (err) {
+                res.send(err);
+            }
+            res.status(200).send({
+                code: 200,
+                error: global.status._200,
+            });
+        })
     });
 }
