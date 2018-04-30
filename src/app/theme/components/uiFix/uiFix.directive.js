be0c077a95b059ebfe49fb58998ac10cd3d36e86
Ichen <ichenJSW@jswpac.com>
2018-04-30 15:22:09 +0800

    minor issue modify

--- src/app/theme/components/uiFix/uiFix.directive.js
+++ src/app/theme/components/uiFix/uiFix.directive.js
@@ -13,6 +13,7 @@
                 link: function (scope, elem, attrs, ngModel) {
 
                     ngModel.$validators.arrayRequired = function (modelValue, viewValue) {
+                        if (modelValue === undefined) return false;
                         return (modelValue.length > 0 ? true : false);
                     };
 
