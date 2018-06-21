8e29468c733da0cf056052b0480d72e843094fc9
Ichen <ichenJSW@jswpac.com>
2018-06-21 19:45:07 +0800

    work add confirm form add.

---
+++ src/server/restfulAPI/forms/workAddConfirmForm/workAddConfirmForm.routes.js
@@ -0,0 +1,68 @@
+var WorkAddConfirmForm = require('../../models/workAddConfirmForm');
+
+module.exports = function (app) {
+    'use strict';
+    // ----- define routes
+
+    // create Form
+    app.post(global.apiUrl.post_create_work_add_confirm_form, function (req, res) {
+        if (req.body.oldFormID !== undefined) {
+            WorkAddConfirmForm.remove({
+                _id: req.body.oldFormID
+            }, function (err) {
+                if (err) {
+                    res.send(err);
+                }
+                WorkAddConfirmForm.create({
+                    creatorDID: req.body.creatorDID,
+                    year: req.body.year,
+                    month: req.body.month,
+                    formTables: JSON.stringify(req.body.formTables)
+                }, function (err) {
+                    if (err) {
+                        res.send(err);
+                    }
+                    res.status(200).send({
+                        code: 200,
+                        error: global.status._200,
+                        // payload: holidayForm,
+                    });
+                })
+            })
+        } else {
+            WorkAddConfirmForm.create({
+                creatorDID: req.body.creatorDID,
+                year: req.body.year,
+                month: req.body.month,
+                formTables: JSON.stringify(req.body.formTables)
+            }, function (err) {
+                if (err) {
+                    res.send(err);
+                }
+                res.status(200).send({
+                    code: 200,
+                    error: global.status._200,
+                    // payload: holidayForm,
+                });
+            })
+        }
+    });
+
+    // fetch
+    app.post(global.apiUrl.post_fetch_work_add_confirm_form_by_user_id, function (req, res) {
+        WorkAddConfirmForm.find({
+            creatorDID: req.body.creatorDID,
+            year: req.body.year,
+            month: req.body.month,
+        }, function (err, formDataResponse) {
+            if (err) {
+                res.send(err);
+            }
+            res.status(200).send({
+                code: 200,
+                error: global.status._200,
+                payload: formDataResponse,
+            });
+        })
+    });
+}
---
+++ src/server/restfulAPI/models/workAddConfirmForm.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+// 加班分配表 文惠專用
+module.exports = mongoose.model('WorkAddConfirmForm', {
+    //填表人
+    creatorDID: {
+        type: String,
+    },
+    // 年分
+    year: {
+        type: Number,
+    },
+    // 月份
+    month: {
+        type: Number,
+    },
+    formTables: {
+        type: String,
+    },
+
+
+});
