28a178bc1f273e782be68c65f12aefb99dfcd48f
Ichen <ichenJSW@jswpac.com>
2018-06-22 15:08:37 +0800

    my weekly date picker supports

---
+++ src/app/pages/myInput/widgets/myWeeklyDatePicker/myWeeklyDatePicker.directive.js
@@ -0,0 +1,29 @@
+/**
+ * @author Ichen.chu
+ * created on 22.06.2018
+ */
+(function () {
+    'use strict';
+
+    angular.module('BlurAdmin.pages.myInput')
+        .directive('myWeeklyDatePicker',
+            [
+                'DateUtil',
+                myWeeklyDatePicker
+            ]);
+
+    /** @ngInject */
+    function myWeeklyDatePicker(DateUtil) {
+        return {
+            replace: true,
+            restrict: 'E',
+            controller: 'MyWeeklyDatePickerCtrl',
+            // template: "<div><h2>我叫{{name}}</h2></div>",
+            link: function ($scope, elem, attrs) {
+                $scope.format = attrs.formatted;
+                $scope.myDT = attrs.default === undefined ? new Date(DateUtil.getShiftDatefromFirstDate(DateUtil.getFirstDayofThisWeek(moment()), 0)) : new Date(attrs.default);
+            },
+            templateUrl: 'app/pages/myInput/widgets/myWeeklyDatePicker/myWeeklyDatePicker.html',
+        };
+    }
+})();
