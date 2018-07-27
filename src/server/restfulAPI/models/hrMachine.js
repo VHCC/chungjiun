8ae69c59a7f1ffe4c7b294411f5345446bca7b23
IchenWindows <ichenJSW@jswpac.com>
2018-07-27 14:25:49 +0800

    OverwriteModelError issue.

--- src/server/restfulAPI/models/hrMachine.js
+++ src/server/restfulAPI/models/hrMachine.js
@@ -3,26 +3,33 @@ var mongoose = require('mongoose');
 module.exports = function(fileDate) {
 
     var modelName = 'HrMachine' + "_" + fileDate;
-    return mongoose.model(modelName, {
-        // 地區
-        location: {
-            type: String,
-        },
-        // 編號
-        did: {
-            type: String,
-        },
-        date: {
-            type: String,
-        },
-        time: {
-            type: String,
-        },
-        workType: {
-            type: String,
-        },
-        printType: {
-            type: String,
-        }
-    });
+
+    var mongoModel
+    try {
+        mongoModel = mongoose.model(modelName);
+    } catch (error) {
+        mongoModel = mongoose.model(modelName, {
+            // 地區
+            location: {
+                type: String,
+            },
+            // 編號
+            did: {
+                type: String,
+            },
+            date: {
+                type: String,
+            },
+            time: {
+                type: String,
+            },
+            workType: {
+                type: String,
+            },
+            printType: {
+                type: String,
+            }
+        });
+    }
+    return mongoModel
 }
