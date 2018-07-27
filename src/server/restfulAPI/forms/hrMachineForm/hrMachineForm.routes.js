e36e1b647ee33afb930e558905e82c10b832f38b
IchenWindows <ichenJSW@jswpac.com>
2018-07-27 14:16:19 +0800

    load multi-date

--- src/server/restfulAPI/forms/hrMachineForm/hrMachineForm.routes.js
+++ src/server/restfulAPI/forms/hrMachineForm/hrMachineForm.routes.js
@@ -1,6 +1,6 @@
-var fileDate = '20180712';
+var moment = require('moment');
 
-var HrMachineForm = require('../../models/hrMachine')(fileDate);
+
 
 module.exports = function (app) {
     'use strict';
@@ -8,18 +8,44 @@ module.exports = function (app) {
 
     // fetch
     app.post(global.apiUrl.post_fetch_hrmachine_data_by_machine_did, function (req, res) {
-        console.log(req.body);
-        HrMachineForm.find({
-            did: req.body.machineDID,
-        }, function (err, formDataResponse) {
-            if (err) {
-                res.send(err);
-            }
-            res.status(200).send({
-                code: 200,
-                error: global.status._200,
-                payload: formDataResponse,
-            });
-        })
+        var resultArry = [];
+        // console.log(req.body);
+        var startDate = moment(req.body.startDate);
+
+        var endDate = moment(req.body.endDate);
+
+        var daysCount = endDate.diff(startDate, 'days');
+        console.log(daysCount);
+
+        var resultCount = 0;
+
+        for (var index = 0; index < daysCount + 1; index ++) {
+            var workDate = startDate.add(index, 'days').format('YYYYMMDD');
+            console.log(workDate);
+
+            var HrMachineForm = require('../../models/hrMachine')(workDate);
+
+            HrMachineForm.find({
+                did: req.body.machineDID,
+            }, function (err, formDataResponse) {
+                resultCount++;
+                if (err) {
+                    res.send(err);
+                }
+                // console.log(formDataResponse);
+                resultArry.push(formDataResponse);
+                // console.log(resultCount);
+
+                if (resultCount === daysCount + 1) {
+                    // console.log(resultArry);
+                    res.status(200).send({
+                        code: 200,
+                        error: global.status._200,
+                        payload: resultArry,
+                    });
+                }
+            })
+        }
     });
+
 }
