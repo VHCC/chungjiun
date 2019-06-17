fc0fd329a81e430e4c48656e671c0b53b5797a26
Ichen.tsu <r99521320@gmail.com>
2019-06-17 17:12:09 +0800

    role 100 bugs

--- src/app/pages/myForms/workHourTableForm/workHourTableFormCtrl.js
+++ src/app/pages/myForms/workHourTableForm/workHourTableFormCtrl.js
@@ -2735,7 +2735,6 @@
             var relatedMembers = [];
 
             switch($scope.roleType) {
-                case "100":
                 case "6": // 主任
                 case "2": // 經理
                     Project.getProjectRelatedToManager(formData)
@@ -2776,6 +2775,43 @@
                         })
                     break;
                 case "100": //顯示行政審查人員
+                    Project.getProjectRelatedToManager(formData)
+                        .success(function (relatedProjects) {
+                            // console.log(relatedProjects);
+                            for(var index = 0; index < relatedProjects.length; index ++) {
+                                // 相關專案
+                                managersRelatedProjects.push(relatedProjects[index]._id);
+                                // console.log(relatedMembers);
+                                //主辦
+                                if (relatedProjects[index].majorID !== undefined
+                                    && relatedProjects[index].majorID !== ""
+                                    && relatedProjects[index].majorID !== null
+                                    && !relatedMembers.includes(relatedProjects[index].majorID)) {
+                                    // console.log("AA");
+                                    // console.log(relatedProjects[index].majorID);
+                                    relatedMembers.push(relatedProjects[index].majorID);
+                                }
+                                //協辦
+                                if (relatedProjects[index].workers.length !== 0) {
+                                    // console.log("BB");
+                                    for (var subIndex = 0; subIndex < relatedProjects[index].workers.length; subIndex ++) {
+                                        if (relatedProjects[index].workers[subIndex] !== undefined
+                                            && relatedProjects[index].workers[subIndex] !== ""
+                                            && relatedProjects[index].workers[subIndex] !== null
+                                            && !relatedMembers.includes(relatedProjects[index].workers[subIndex])) {
+                                            // console.log(relatedProjects[index].workers[subIndex]);
+                                            relatedMembers.push(relatedProjects[index].workers[subIndex]);
+                                        }
+                                    }
+                                }
+                            }
+                            if (!relatedMembers.includes(cookies.get('userDID'))) {
+                                relatedMembers.push(cookies.get('userDID'));
+                            }
+                            $scope.mainRelatedMembers = relatedMembers;
+                            $scope.showRelatedMembersTableReview(typeManager);
+                        })
+                    break;
                     // 行政總管跟每個人都有關
 
                     // 所有人，對照資料
