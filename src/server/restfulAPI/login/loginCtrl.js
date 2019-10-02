d3e6b47bdb5bb71fcb18911807de332fd540d9ea
Ichen.tsu <r99521320@gmail.com>
2019-10-02 14:07:50 +0800

    login success url

--- src/server/restfulAPI/login/loginCtrl.js
+++ src/server/restfulAPI/login/loginCtrl.js
@@ -70,7 +70,7 @@
 
                         console.log('cookies.username= ' + cookies.username);
                         console.log('cookies.username= ' + cookies.get('username'));
-                        window.location.href = 'http://localhost:4000';
+                        window.location.href = 'https://vhc.ichenprocin.dsmynas.com';
 
                         scope.todos = data; // assign our new list of todos
                     })
