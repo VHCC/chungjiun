0e29529eea0419d63b16bf65fc6a11393cd840b1
Ichen.tsu <r99521320@gmail.com>
2019-10-15 16:32:36 +0800

    hide official menu

--- src/app/menus/cgOfficialDoc/myOfficialDoc.module.js
+++ src/app/menus/cgOfficialDoc/myOfficialDoc.module.js
@@ -11,59 +11,59 @@
     /** @ngInject */
     function routeConfig($stateProvider, $urlRouterProvider) {
         $stateProvider
-            .state('cgOfficialDoc',
-                {
-                    url: '/cgOfficialDoc',
-                    template: '<ui-view autoscroll="true" autoscroll-body-top></ui-view>',
-                    abstract: true,
-                    title: '公文管理',
-                    sidebarMeta: {
-                        icon: 'ion-compose',
-                        order: global.pages.cgKPI,
-                    },
-                })
-            .state('cgOfficialDoc.100',
-                {
-                    url: '/officialDocList',
-                    templateUrl: 'app/pages/myProject/listProjectAll/listProjectAll.html',
-                    controller: 'listOfficialDocCtrl',
-                    title: '公文列表',
-                    // accessLevel: [9999],
-                    sidebarMeta: {
-                        order: global.pages.cgOfficialDoc_list,
-                    },
-                })
-            .state('cgOfficialDoc.200',
-                {
-                    url: '/officialDocReceive',
-                    templateUrl: 'app/pages/officialDoc/receiveOfficialDoc/receiveOfficialDocHomePage.html',
-                    controller: 'receiveOfficialDocCtrl',
-                    controllerAs: 'receiveOfficialDocCtrlVm',
-                    title: '收文作業',
-                    // accessLevel: [9999],
-                    sidebarMeta: {
-                        order: global.pages.cgOfficialDoc_receive,
-                    },
-                })
-            .state('cgOfficialDoc.300',
-                {
-                    url: '/300',
-                    templateUrl: 'app/pages/myNewPage/empty.html',
-                    title: '公文辦理',
-                    // accessLevel: [9999],
-                    sidebarMeta: {
-                        order: global.pages.cgOfficialDoc_300,
-                    },
-                })
-            .state('cgOfficialDoc.400',
-                {
-                    url: '/400',
-                    templateUrl: 'app/pages/myNewPage/empty.html',
-                    title: '發文作業',
-                    // accessLevel: [9999],
-                    sidebarMeta: {
-                        order: global.pages.cgOfficialDoc_400,
-                    },
-                })
+            // .state('cgOfficialDoc',
+            //     {
+            //         url: '/cgOfficialDoc',
+            //         template: '<ui-view autoscroll="true" autoscroll-body-top></ui-view>',
+            //         abstract: true,
+            //         title: '公文管理',
+            //         sidebarMeta: {
+            //             icon: 'ion-compose',
+            //             order: global.pages.cgKPI,
+            //         },
+            //     })
+            // .state('cgOfficialDoc.100',
+            //     {
+            //         url: '/officialDocList',
+            //         templateUrl: 'app/pages/myProject/listProjectAll/listProjectAll.html',
+            //         controller: 'listOfficialDocCtrl',
+            //         title: '公文列表',
+            //         // accessLevel: [9999],
+            //         sidebarMeta: {
+            //             order: global.pages.cgOfficialDoc_list,
+            //         },
+            //     })
+            // .state('cgOfficialDoc.200',
+            //     {
+            //         url: '/officialDocReceive',
+            //         templateUrl: 'app/pages/officialDoc/receiveOfficialDoc/receiveOfficialDocHomePage.html',
+            //         controller: 'receiveOfficialDocCtrl',
+            //         controllerAs: 'receiveOfficialDocCtrlVm',
+            //         title: '收文作業',
+            //         // accessLevel: [9999],
+            //         sidebarMeta: {
+            //             order: global.pages.cgOfficialDoc_receive,
+            //         },
+            //     })
+            // .state('cgOfficialDoc.300',
+            //     {
+            //         url: '/300',
+            //         templateUrl: 'app/pages/myNewPage/empty.html',
+            //         title: '公文辦理',
+            //         // accessLevel: [9999],
+            //         sidebarMeta: {
+            //             order: global.pages.cgOfficialDoc_300,
+            //         },
+            //     })
+            // .state('cgOfficialDoc.400',
+            //     {
+            //         url: '/400',
+            //         templateUrl: 'app/pages/myNewPage/empty.html',
+            //         title: '發文作業',
+            //         // accessLevel: [9999],
+            //         sidebarMeta: {
+            //             order: global.pages.cgOfficialDoc_400,
+            //         },
+            //     })
     }
 })();
