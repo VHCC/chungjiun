c736c650a39d66f1a1e45a1691bcb780267fab08
Ichen.tsu <r99521320@gmail.com>
2019-12-11 16:10:13 +0800

    list official docs info

--- src/app/pages/officialDoc/listOfficialDoc/listOfficialDocCtrl.js
+++ src/app/pages/officialDoc/listOfficialDoc/listOfficialDocCtrl.js
@@ -2,15 +2,41 @@
     'user strict';
 
     angular.module('BlurAdmin.pages.cgOfficialDoc')
+        .service('intiOfficialDocAllService', function ($http, $cookies) {
+
+            var promise = $http.get('/api/get_official_doc_fetch_all_item')
+                .success(function (allOfficialDocItems) {
+                    return allOfficialDocItems;
+                });
+            return promise;
+        })
         .controller('listOfficialDocCtrl',
             [
                 '$scope',
+                '$filter',
                 '$cookies',
+                'User',
+                'OfficialDocUtil',
+                'OfficialDocVendorUtil',
+                '$compile',
+                'intiOfficialDocAllService',
                 function (scope,
-                          cookies) {
+                          filter,
+                          $cookies,
+                          User,
+                          OfficialDocUtil,
+                          OfficialDocVendorUtil,
+                          $compile,
+                          intiOfficialDocAllService) {
                     return new ListOfficialDocCtrl(
                         scope,
-                        cookies
+                        filter,
+                        $cookies,
+                        User,
+                        OfficialDocUtil,
+                        OfficialDocVendorUtil,
+                        $compile,
+                        intiOfficialDocAllService
                     );
                 }])
     ;
@@ -18,9 +44,97 @@
     /**
      * @ngInject
      */
-    function ListOfficialDocCtrl(scope,
-                                 cookies) {
+    function ListOfficialDocCtrl($scope,
+                                 $filter,
+                                 $cookies,
+                                 User,
+                                 OfficialDocUtil,
+                                 OfficialDocVendorUtil,
+                                 $compile,
+                                 intiOfficialDocAllService) {
         console.log('ListOfficialDocCtrl');
+
+        intiOfficialDocAllService.then(function (resp) {
+            console.log(resp.data);
+            $scope.officialDocItems = resp.data.payload;
+            console.log($scope.officialDocItems);
+            $scope.officialDocItems.slice(0, resp.data.payload.length);
+
+            angular.element(
+                document.getElementById('includeHead'))
+                .append($compile(
+                    "<div ba-panel ba-panel-title=" +
+                    "'所有公文列表 - " + resp.data.payload.length + "'" +
+                    "ba-panel-class= " +
+                    "'with-scroll'" + ">" +
+                    "<div " +
+                    "ng-include=\"'app/pages/officialDoc/listOfficialDoc/table/listOfficialTable.html'\">" +
+                    "</div>" +
+                    "</div>"
+                )($scope));
+
+            User.getAllUsers()
+                .success(function (allUsers) {
+                    // console.log(allUsers);
+                    // 經理、主承辦
+                    $scope.allUsers = [];
+                    $scope.allUsers[0] = {
+                        value: "",
+                        name: "None"
+                    };
+                    for (var i = 0; i < allUsers.length; i++) {
+                        $scope.allUsers[i] = {
+                            value: allUsers[i]._id,
+                            name: allUsers[i].name
+                        };
+                    }
+                })
+
+            OfficialDocVendorUtil.fetchOfficialDocVendor()
+                .success(function (response) {
+                    console.log(response);
+                    $scope.allVendors = [];
+                    $scope.allVendors[0] = {
+                        value: "",
+                        name: "None"
+                    };
+                    for (var i = 0; i < response.payload.length; i++) {
+                        $scope.allVendors[i] = {
+                            value: response.payload[i]._id,
+                            name: response.payload[i].vendorName
+                        };
+                    }
+                })
+        })
+
+
+        $scope.showDocType = function (type) {
+            return OfficialDocUtil.getDocType(type);
+        }
+
+        $scope.showCharger = function (officialItem) {
+            var selected = [];
+            if ($scope.allUsers === undefined) return;
+            if (officialItem.chargerDID) {
+                selected = $filter('filter')($scope.allUsers, {
+                    value: officialItem.chargerDID
+                });
+            }
+            return selected.length ? selected[0].name : 'Not Set';
+        }
+
+        $scope.showVendorName = function (officialItem) {
+            // console.log(officialItem);
+            // console.log($scope.allVendors);
+            var selected = [];
+            if ($scope.allVendors === undefined) return;
+            if (officialItem.vendorDID) {
+                selected = $filter('filter')($scope.allVendors, {
+                    value: officialItem.vendorDID
+                });
+            }
+            return selected.length ? selected[0].name : 'Not Set';
+        }
     }
 
 })();
