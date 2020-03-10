e829ad9c97db13795efc03defacf292ad9f259cd
Ichen.tsu <r99521320@gmail.com>
2020-03-10 14:42:31 +0800

    [Feature] mail send new specs

--- src/app/mailCheck.js
+++ src/app/mailCheck.js
@@ -37,62 +37,15 @@ userModel.find({}, function (err, allUsers) {
                 cjMail: allUsers[i].cjMail,
             };
         }
-
     }
 })
 
-// userModel.find({}, function (err, allUsers) {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         console.log("user counts= " + allUsers.length);
-//         allUsersCache = [];
-//         allUsersCache[0] = {
-//             value: "",
-//             name: "None"
-//         };
-//         for (var i = 0; i < allUsers.length; i++) {
-//             allUsersCache[i] = {
-//                 value: allUsers[i]._id,
-//                 name: allUsers[i].name,
-//                 cjMail: allUsers[i].cjMail,
-//             };
-//         }
-//
-//         officialDocModel.find(
-//             {
-//                 dueDate: dueDate,
-//                 isDocClose: false,
-//                 type: 0
-//
-//             }, function (err, items) {
-//                 if (err) {
-//                     console.log(err);
-//                 } else {
-//                     console.log("docs= " + items.length);
-//                     for (var index = 0; index < items.length; index++) {
-//                         if (items[index].signerDID) {
-//                             console.log(items[index].signerDID);
-//
-//                             var output = allUsersCache.find(function (item) {
-//                                 return item.value == items[index].signerDID;
-//                             })
-//
-//
-//                         }
-//                     }
-//
-//
-//                 }
-//             })
-//     }
-// })
-
 var defaultMail_1;
 var defaultMail_2;
+var defaultMail_3;
 
 schedule.scheduleJob('0 00 08 * * *', function() {
-// schedule.scheduleJob('* * * * * *', function() {
+// schedule.scheduleJob('10 29 14 * * *', function () {
 
     defaultMail_1 = allUsersCache.find(function (item) {
         return item.name == "林佳樞";
@@ -102,11 +55,15 @@ schedule.scheduleJob('0 00 08 * * *', function() {
         return item.name == "蕭鈺樺";
     })
 
+    defaultMail_3 = allUsersCache.find(function (item) {
+        return item.name == "陳瑞鳳";
+    })
 
-    checkDueDateMail ();
-    // console.log(defaultMail_1);
-    // console.log(defaultMail_2);
-    console.log('scheduleCronstyle:' + new Date());
+
+    checkNoHandleMail(3, 1);
+    checkDueDateMail(1, 0);
+    checkDueDateMail(-1, 2);
+    console.log('schedule: ' + new Date());
 });
 
 function getDocDivisionName(division) {
@@ -120,14 +77,19 @@ function getDocDivisionName(division) {
         case 3:
             return "D";
         case 4:
-            return "P"
+            return "P";
     }
 }
 
 
-function checkDueDateMail () {
+/**
+ * @param days
+ * @param targetType 0= singer, 1= handler, 2=小鳳
+ */
 
-    dueDate = moment().add(3, 'days').format("YYYY/MM/DD");
+function checkDueDateMail(days, targetType) {
+
+    dueDate = moment().add(days, 'days').format("YYYY/MM/DD");
     console.log(" checkDueDateMail: ------ dueDate= " + dueDate + " ------ ");
 
     officialDocModel.find(
@@ -140,16 +102,14 @@ function checkDueDateMail () {
             if (err) {
                 console.log(err);
             } else {
-                // console.log(items);
                 console.log("doc counts= " + items.length);
-                // console.log(today);
 
                 var counter = 1;
 
                 var mailIndex = 0;
 
                 var syncSendMail = false;
-                var j = schedule.scheduleJob('* * * * * *', function(){
+                var j = schedule.scheduleJob('* * * * * *', function () {
 
                     if (mailIndex < items.length) {
 
@@ -161,14 +121,10 @@ function checkDueDateMail () {
                                 console.log("*** signerDID= " + items[mailIndex].signerDID);
 
                                 var signer = allUsersCache.find(function (item) {
-                                    // console.log(item);
-                                    // console.log(items[mailIndex].signerDID);
                                     return item.value == items[mailIndex].signerDID;
                                 })
 
                                 var handler = allUsersCache.find(function (item) {
-                                    // console.log(item);
-                                    // console.log(items[mailIndex].signerDID);
                                     return item.value == items[mailIndex].handlerDID;
                                 })
 
@@ -179,18 +135,27 @@ function checkDueDateMail () {
                                 if (signer.cjMail == undefined || signer.cjMail == null) {
                                     sendTarget = defaultMail_1.name + ' <' + defaultMail_1.cjMail + '>;' +
                                         defaultMail_2.name + ' <' + defaultMail_2.cjMail + '>'
-                                }  else {
+                                } else {
                                     sendTarget = signer.name + ' <' + signer.cjMail + '>';
                                 }
 
-                                console.log(sendTarget);
+                                var subject_tail = ' 即將到期，請留意';
+                                var html_content = '最後期限即將到期，請注意';
+
+                                if (targetType == 2) {
+                                    sendTarget =  defaultMail_3.name + ' <' + defaultMail_3.cjMail + '>';
+                                    subject_tail = ' 已經過期，且尚未歸檔，請留意';
+                                    html_content = '已經過期，且尚未歸檔，請注意';
+                                }
+
+                                console.log("sendTarget:" + sendTarget);
 
                                 mailTransport.sendMail({
                                     from: 'ERM System <0973138343@chongjun.tw>',
                                     to: sendTarget,
-                                    subject: '公文 ' + getDocDivisionName(items[mailIndex].docDivision) + items[mailIndex].archiveNumber + ' 即將到期，請留意',
+                                    subject: '公文 ' + getDocDivisionName(items[mailIndex].docDivision) + items[mailIndex].archiveNumber + subject_tail,
                                     html: '<h1>公文 ' + getDocDivisionName(items[mailIndex].docDivision) + items[mailIndex].archiveNumber + '</h1>' +
-                                    '<p> 最後期限即將到期，請注意</p><br/>' +
+                                    '<p> ' + html_content + ' </p><br/>' +
                                     '<p> 到期日：' + items[mailIndex].dueDate + '</p>' +
                                     '<p> 待辦人：' + handler.name + '</p>'
 
@@ -207,7 +172,7 @@ function checkDueDateMail () {
                             } else {
                                 console.log(mailIndex + " doc has no signer");
                                 syncSendMail = false;
-                                mailIndex ++;
+                                mailIndex++;
                             }
                         }
 
@@ -220,6 +185,120 @@ function checkDueDateMail () {
                     // console.log('定時器觸發次數：' + counter);
                     counter++;
 
+                });
+
+            }
+        })
+}
+
+
+function checkNoHandleMail(days, targetType) {
+
+    dueDate = moment().add(days, 'days').format("YYYY/MM/DD");
+    console.log(" checkNoHandleMail: ------ dueDate= " + dueDate + " ------ ");
+
+    officialDocModel.find(
+        {
+            dueDate: dueDate,
+            isDocClose: false,
+            "stageInfo": { $size: 1 },
+            type: 0
+
+        }, function (err, items) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("doc counts= " + items.length);
+
+                var counter = 1;
+
+                var mailIndex = 0;
+
+                var syncSendMail = false;
+                var j = schedule.scheduleJob('* * * * * *', function(){
+
+                    if (mailIndex < items.length) {
+
+                        if (!syncSendMail) {
+                            syncSendMail = true;
+
+                            var mailTarget;
+
+                            switch (targetType) {
+                                case 0:
+                                    if (items[mailIndex].signerDID) {
+                                        var signer = allUsersCache.find(function (item) {
+                                            return item.value == items[mailIndex].signerDID;
+                                        })
+                                        console.log("*** signerDID= " + items[mailIndex].signerDID);
+                                        mailTarget = signer;
+                                    } else {
+                                        console.log(mailIndex + " doc has no signer");
+                                        syncSendMail = false;
+                                        mailIndex ++;
+                                    }
+                                    break;
+                                case 1:
+                                    if (items[mailIndex].chargerDID) {
+                                        var charger = allUsersCache.find(function (item) {
+                                            return item.value == items[mailIndex].chargerDID;
+                                        })
+                                        console.log("*** chargerDID= " + items[mailIndex].chargerDID);
+                                        mailTarget = charger;
+                                    } else {
+                                        console.log(mailIndex + " doc has no chargerDID");
+                                        syncSendMail = false;
+                                        mailIndex ++;
+                                    }
+                                    break;
+                                case 2:
+                                    break;
+                            }
+
+                            console.log("*** doc ID= " + getDocDivisionName(items[mailIndex].docDivision) + items[mailIndex].archiveNumber);
+
+                            console.log(mailTarget);
+
+                            var sendTarget = ""
+
+                            if (mailTarget.cjMail == undefined || mailTarget.cjMail == null) {
+                                sendTarget = defaultMail_1.name + ' <' + defaultMail_1.cjMail + '>;' +
+                                    defaultMail_2.name + ' <' + defaultMail_2.cjMail + '>'
+                            }  else {
+                                sendTarget = mailTarget.name + ' <' + mailTarget.cjMail + '>';
+                            }
+
+                            console.log("sendTarget:" + sendTarget);
+
+                            mailTransport.sendMail({
+                                from: 'ERM System <0973138343@chongjun.tw>',
+                                to: sendTarget,
+                                subject: '公文 ' + getDocDivisionName(items[mailIndex].docDivision) + items[mailIndex].archiveNumber + ' 請盡快處理',
+                                html: '<h1>公文 ' + getDocDivisionName(items[mailIndex].docDivision) + items[mailIndex].archiveNumber + '</h1>' +
+                                '<p> 最後期限即將到期，請注意</p><br/>' +
+                                '<p> 到期日：' + items[mailIndex].dueDate + '</p>' +
+                                '<p> 承辦人：' + mailTarget.name + '</p>'
+
+                            }, function(err){
+                                if(err){
+                                    console.log('Unable to send email: ' + err);
+                                    syncSendMail = false;
+                                } else {
+                                    console.log('Send Successfully: no.' + mailIndex + ', archiveNumber= ' + getDocDivisionName(items[mailIndex].docDivision) + items[mailIndex].archiveNumber);
+                                    syncSendMail = false;
+                                    mailIndex ++;
+                                }
+                            });
+                        }
+
+
+                    } else {
+                        console.log("doc counts= " + items.length);
+                        j.cancel();
+                    }
+
+                    // console.log('定時器觸發次數：' + counter);
+                    counter++;
                 });
 
             }
