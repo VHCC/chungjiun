a87346832b855a0bc2df362624f3cc258b4ebf40
Ichen.tsu <r99521320@gmail.com>
2022-04-11 14:36:03 +0800

    bug fixed

--- src/app/pages/myInput/widgets/myTimePickerSingle/myTimePickerSingleCtrl.js
+++ src/app/pages/myInput/widgets/myTimePickerSingle/myTimePickerSingleCtrl.js
@@ -19,13 +19,11 @@
                                     $filter,
                                     $cookies,
                                     toastr) {
-
         $scope.options = {
             showWeeks: false
         };
 
         $scope.aaaa = function (event) {
-            console.log(event)
             if (event.keyCode == 229) {
                 toastr.error('輸入法異常', '（時間輸入格式錯誤，可能有輸入到中文、注音、英文字母、請重新整理後再次輸入）');
                 return;
@@ -33,7 +31,6 @@
         }
 
         $scope.changeTime = function (type) {
-            console.log(type)
             switch(type) {
                 case 0:
                     $scope.$parent.item.start_time = $scope.time;
@@ -42,8 +39,6 @@
                     $scope.$parent.item.end_time = $scope.time;
                     break;
             }
-            console.log($scope.$parent.item);
-            console.log($scope);
         }
 
     }
